7481f2af5a3d9b9b8129f0ef782b787de0ea9409
Michal <michalgrzegorczyk04@gmail.com>
2023-06-16 19:40:16 +0200

    add auth guard

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,31 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+class AuthGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
+
+export const canActivateTraining: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  return inject(AuthGuard).canActivate();
+};
