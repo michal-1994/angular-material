8c30157aae472df4f672f49b15a0be30ee6dd8e4
Michal <michalgrzegorczyk04@gmail.com>
2023-06-28 14:04:23 +0200

    creating a shared module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
+
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    provideAuth(() => getAuth()),
-  ],
+  imports: [ReactiveFormsModule, SharedModule, provideAuth(() => getAuth())],
   exports: [],
 })
 export class AuthModule {}
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,8 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
@@ -17,13 +15,7 @@ import { TrainingComponent } from './training.component';
     PastTrainingComponent,
     StopTrainingComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-  ],
+  imports: [SharedModule],
   exports: [],
 })
 export class TrainingModule {}
